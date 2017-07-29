933e159c36ac88e55052036a82d4b055a023f34f
alx77-demo <alx77@bk.ru>
2017-07-29 16:51:44 +0300

    step 3

--- DemoProject1/src/DemoApp.java
+++ DemoProject1/src/DemoApp.java
@@ -1,9 +1,9 @@
 
 public class DemoApp {
-
+	static WonderfulManager wm = new WonderfulManager();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello from Demo!");
+		wm.giveMeWonder();
 	}
 
 }
