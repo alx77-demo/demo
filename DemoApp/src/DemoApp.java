a4219b0927aedb84d1254987dc0a7b24fe2ab86c
alx77-demo <alx77@bk.ru>
2017-07-29 21:39:02 +0300

    one more time 2

--- DemoApp/src/DemoApp.java
+++ DemoApp/src/DemoApp.java
@@ -2,9 +2,11 @@
 public class DemoApp {
 
 	public static void main(String[] args) {
+		OutputManager om = new OutputManager();
+		om.echo();
 		// TODO Auto-generated method stub
 		System.out.println("Hi there!");
-		System.out.println("It's me.");
+		System.out.println("912873192387");
 		System.out.println("One more time");
 	}
 
