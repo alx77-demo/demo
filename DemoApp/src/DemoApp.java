87c4aa2939d94ec5632fc2c84ae5c464a3a97981
alx77-demo <alx77@bk.ru>
2017-07-29 21:18:11 +0300

    one more line

--- DemoApp/src/DemoApp.java
+++ DemoApp/src/DemoApp.java
@@ -4,6 +4,7 @@ public class DemoApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi there!");
+		System.out.println("It's me.");
 	}
 
 }
