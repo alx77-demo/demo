4ebbf97cb1e7996c3b47e819cd0b7f9056923132
dmitrydomanski <dzmitry.damanski@gmail.com>
2017-07-29 21:40:54 +0200

    thoughtful commit

--- 20170728_GitHub_Domanski/src/Printer.java
+++ 20170728_GitHub_Domanski/src/Printer.java
@@ -2,8 +2,10 @@
 public class Printer {
 
 	public static void main(String[] args) {
+		OutputManager om = new OutputManager();
+		om.echo();
 		System.out.println("C'mon everybody");
-		System.out.println("C'mon now");
+		System.out.println("C'mon now x 2");
 		System.out.println("C'mon and dance");
 
 	}
