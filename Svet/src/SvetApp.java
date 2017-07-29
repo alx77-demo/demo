ffc32f87b1715c82bc5ddf23dd311da4712a91fe
esvet78 <esvet78@gmail.com>
2017-07-29 21:38:41 +0300

    one more time 2

--- Svet/src/SvetApp.java
+++ Svet/src/SvetApp.java
@@ -2,8 +2,10 @@
 public class SvetApp {
 
 	public static void main(String[] args) {
+		OutputManager om = new OutputManager();
+		om.echo();
 		System.out.println("Hi There!");
-		System.out.println("It's me");
+		System.out.println("924545452");
 		System.out.println("One more time");
 	}
 
