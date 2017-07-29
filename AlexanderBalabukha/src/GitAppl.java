6d97246df330b3b0c1f974ce4b8ec1e51e28f835
Alexander.Balabukha <alexander.balabukha@gmail.com>
2017-07-29 21:28:32 +0300

    one more time

--- AlexanderBalabukha/src/GitAppl.java
+++ AlexanderBalabukha/src/GitAppl.java
@@ -2,8 +2,9 @@
 public class GitAppl {
 
 	public static void main(String[] args) {
-System.out.println("ssasdasd");
-System.out.println("hello world");
+		System.out.println("ssasdasd");
+		System.out.println("hello world");
+		System.out.println("Hi to all");
 	}
 
 }
