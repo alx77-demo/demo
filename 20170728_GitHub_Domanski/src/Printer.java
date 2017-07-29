cf3cd9df336a83da57528e00190fdfbb9350615c
dmitrydomanski <dzmitry.damanski@gmail.com>
2017-07-29 21:30:29 +0200

    commit for test

--- 20170728_GitHub_Domanski/src/Printer.java
+++ 20170728_GitHub_Domanski/src/Printer.java
@@ -4,6 +4,7 @@ public class Printer {
 	public static void main(String[] args) {
 		System.out.println("C'mon everybody");
 		System.out.println("C'mon now");
+		System.out.println("C'mon and dance");
 
 	}
 
