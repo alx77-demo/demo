07ff9343eb092e7422fa93bef36b7d61e1f81e2a
d00rsfan <yura_doorsfan@mail.ru>
2017-07-29 21:56:25 +0300

    mycomm

--- DemoApp/src/DemoApp.java
+++ DemoApp/src/DemoApp.java
@@ -6,7 +6,7 @@ public class DemoApp {
 		om.echo();
 		// TODO Auto-generated method stub
 		System.out.println("Hi there!");
-		System.out.println("912873192387");
+		System.out.println("doorsfan912873192387");
 		System.out.println("One more time");
 		
 				
