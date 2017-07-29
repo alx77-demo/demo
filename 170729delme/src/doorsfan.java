8562391b452d47ccd4f480fcd370a9c3ce626512
d00rsfan <yura_doorsfan@mail.ru>
2017-07-29 21:40:00 +0300

    commit21:38

--- 170729delme/src/doorsfan.java
+++ 170729delme/src/doorsfan.java
@@ -2,8 +2,11 @@
 public class doorsfan {
 
 	public static void main(String[] args) {
+		OutputManager om = new OutputManager();
+		om.echo();
 System.out.println("helo lekulyam");
 System.out.println("legitraot");
+System.out.println("21:34");
 	}
 
 }
