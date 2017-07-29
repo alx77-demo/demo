77dc9939d910b2f4284e5c169321a0ace27ad78f
esvet78 <esvet78@gmail.com>
2017-07-29 21:28:20 +0300

    one more commit

--- Svet/src/SvetApp.java
+++ Svet/src/SvetApp.java
@@ -4,6 +4,7 @@ public class SvetApp {
 	public static void main(String[] args) {
 		System.out.println("Hi There!");
 		System.out.println("It's me");
+		System.out.println("One more time");
 	}
 
 }
