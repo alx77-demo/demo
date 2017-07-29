fd8e77d192e43605863e30af78ecaddd80161998
Gantverg <gantvergroman@gmail.com>
2017-07-29 21:28:41 +0300

    Add "Hi again"

--- Gantverg_Git_Demo_Project/src/HelloWorldAppl.java
+++ Gantverg_Git_Demo_Project/src/HelloWorldAppl.java
@@ -4,6 +4,7 @@ public class HelloWorldAppl {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("Good buy, world!");
+		System.out.println("Hi again!");
 	}
 
 }
