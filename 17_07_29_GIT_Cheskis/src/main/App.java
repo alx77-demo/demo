1a60c226658c47c9ba4d12d7e07be443705f50e1
VladimirCheskis <vcheskis@mail.ru>
2017-07-29 21:39:44 +0300

    misc

--- 17_07_29_GIT_Cheskis/src/main/App.java
+++ 17_07_29_GIT_Cheskis/src/main/App.java
@@ -5,7 +5,10 @@ public class App {
 	public static void main(String[] args) {
 	
 		System.out.println("Hello, world?");
-		System.out.println("I use git! Brunch.");
+		System.out.println("I use git! Brunch 111.");
+		
+		Meneger m = new Meneger();
+		m.echo();
 
 	}
 
